f248224612c35bcd53dce6c7504a1bf96e65a559
Yevhenii <yevhenii.katsimon@gmail.com>
2025-01-07 18:01:23 +0200

    Refactor global styles, enhance HomePage layout, and add Footer component with styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import style from "./home.module.css";
+
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <div className={style.container}>
+      <div className={style.textContainer}>
+        <h1 className={style.title}>Lorem, ipsum dolor.</h1>
+        <p className={style.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+          assumenda consectetur officia quaerat! Harum, dicta.
+        </p>
+        <div className={style.buttons}>
+          <button className={style.button}>Lorem, ipsum.</button>
+          <button className={style.button}>Lorem.</button>
+        </div>
+        <div className={style.brands}>
+          <Image src="/brands.png" alt="" fill className={style.brandImg} />
+        </div>
+      </div>
+      <div className={style.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={style.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import style from "./footer.module.css";
+
 const Footer = () => {
-  return <> Footer </>;
+  return (
+    <div className={style.container}>
+      <div className={style.logo}>Logo</div>
+      <div className={style.text}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
